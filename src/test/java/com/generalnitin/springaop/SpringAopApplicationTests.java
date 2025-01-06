fd7ee5160133ec7149bc94568cf86d08ea0f8d44
nitinkumar-na <nitin.kumar@netapp.com>
2025-01-06 16:52:51 +0530

    feat: initial commit

---
+++ src/test/java/com/generalnitin/springaop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generalnitin.springaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
